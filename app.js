06952e16ef821a25b3f0aa60af20ffc1ceeabfc1
UsuallyChris <schoonc@gmail.com>
2019-04-13 21:41:58 -0700

    Added mongoose connection to mongoDB in app.js

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+// Set up Mongoose connection
+var mongoose = require('mongoose');
+var mongoDb = 'mongodb+srv://dbUser:RriE2oVAePvb4Rvd@cluster0-0qbto.azure.mongodb.net/local_library?retryWrites=true';
+mongoose.connect(mongoDb, { useNewUrlParser: true });
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
