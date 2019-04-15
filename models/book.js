b1edf0881aecdc6782f1bdd9285e28d8421bc994
UsuallyChris <schoonc@gmail.com>
2019-04-14 19:11:30 -0700

    Added book model.

---
+++ models/book.js
@@ -0,0 +1,26 @@
+// require mongoose
+var mongoose = require('mongoose');
+
+// store Schema in a variable
+var Schema = mongoose.Schema;
+
+// define book schema
+var BookSchema = new Schema (
+  {
+    title: {type: String, required: true},
+    author: {type: Schema.Types.ObjectId, ref : 'Author', required: true},
+    summary: {type: String, required: true},
+    isbn: {type: String, required: true},
+    genre: [{type: Schema.Types.ObjectId, ref: 'Genre'}],
+  }  
+)
+
+// define virtual for book's URL
+BookSchema
+  .virtual('url')
+  .length(function () {
+    return '/catalog/book/' + this._id;
+  });
+
+// export schema
+module.exports = mongoose.model('Book', BookSchema);
